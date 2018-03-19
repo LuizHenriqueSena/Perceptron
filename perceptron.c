bcdbf9e36180a55a7b979f7b94aae703e7c3b623
LuizHenriqueSena <coelho.luiz.sena@gmail.com>
2018-03-19 00:55:35 -0400

    .

--- perceptron.c
+++ perceptron.c
@@ -3,7 +3,8 @@
 #include <string.h>
 
 #define xn 2
-#define tamanhoAmostras 8
+#define tamanhoAmostras 10
+#define n 0.5
 
 
 
@@ -14,7 +15,7 @@ int saidaDesejada[tamanhoAmostras];
 const char separadorEntradas[2] = ";";
 const char separadorSaida[2] = "-";
 int funcaoSaida(float u) {
-	if (u<0) return 0;
+	if (u<=0) return -1;
 	else return 1;
 }
 
@@ -31,16 +32,17 @@ int cont = 0;
 	for(cont = 0; cont < xn; cont++) {
 		printf("PESO resultante: %d com valor: %.2f \n", cont, pesosSinapticos[cont]);
 	}
-	printf("limiar de ativacao resultando: %d com valor: %.2f \n", cont, pesosSinapticos[xn]);
+	printf("limiar de ativacao da posicao: %d com valor: %.2f \n", cont, pesosSinapticos[xn]);
 }
 
 void treinaRede() {
 	pesosSinapticos[0]=0.5;
 	pesosSinapticos[1]=0.5;
-	pesosSinapticos[2]= 0;
+	pesosSinapticos[2]= 1;
 	int contAmostras = 0;
 	int cont = 0;
-	float resultado= 0;
+	int cont2 = 0;
+	float resultado = 0;
 	int saida;
 	//computa resposta para entradas
 	for (contAmostras = 0; contAmostras < tamanhoAmostras;contAmostras++) {
@@ -49,15 +51,23 @@ void treinaRede() {
 		}
 		resultado = resultado - pesosSinapticos[xn];
 		saida = funcaoSaida(resultado);
+printf("para a amostra: %d , tem-se  saida: %d e saidaDesejada: %d \n", contAmostras, saida, saidaDesejada[contAmostras]);
 		if(saida != saidaDesejada[contAmostras]) {
 			//ajusta pesos
-			
-		}		
+			for(cont2 = 0; cont2 < xn; cont2++) {
+				pesosSinapticos[cont2] = pesosSinapticos[cont2] + ((n)*(saidaDesejada[contAmostras] + 
+				(-1)*saida)*(amostras[cont2][contAmostras]));	
+			}
+			pesosSinapticos[xn] = pesosSinapticos[xn] + ((n)*(saidaDesejada[contAmostras] + 
+			(-1)*saida)*1);	
+}
 		else {
 			//nao ajusta
 		}
-	}
+	printf("para a amostra: %d , tem-se \n", contAmostras);
 	imprimeRedeResultante();
+	}
+	
 }
 
 void parserAmostras(char *strn, int linha) {
@@ -70,7 +80,7 @@ void parserAmostras(char *strn, int linha) {
    amostras[contx][linha] = atoi(token);
 	printf("VALOR DO PRIMEIRO CAST : %d \n",amostras[contx][linha]);
    contx++;
-   
+
    /* walk through other tokens */
    while( token != NULL ) {
       if(contx >= xn - 1) {
@@ -78,7 +88,10 @@ void parserAmostras(char *strn, int linha) {
 	amostras[contx][linha] = atoi(token);
 	printf("VALOR DO ultimo CAST : %d \n",amostras[contx][linha]);
 	token = strtok(NULL, separadorSaida);
-  	saidaDesejada[linha] = atoi(token);
+  	if (atoi(token) == 0) {
+	saidaDesejada[linha] = -1;
+	}
+	else saidaDesejada[linha] = 1;
 	printf("VALOR Da primeira saida: %d \n",saidaDesejada[linha]);
 	token = strtok(NULL, separadorSaida);
       }
